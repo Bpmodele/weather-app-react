71dbcfc46d05ead7c4c87ed702e057d721550f79
Bpmodele <Bpmodele@users.noreply.github.com>
2024-07-07 12:04:32 -0400

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Madali Navas and is open-sourced on{" "}
+        <a href="https://github.com/Bpmodele/weather-app-react" target="_blank">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
