c721fde695979cffd5e572cac99e3bc33205fd38
Bpmodele <Bpmodele@users.noreply.github.com>
2024-07-07 12:43:01 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Madali Navas and is open-sourced on{" "}
-        <a href="https://github.com/Bpmodele/weather-app-react" target="_blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Madali Navas and is open-sourced on{" "}
+          <a
+            href="https://github.com/Bpmodele/weather-app-react"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
