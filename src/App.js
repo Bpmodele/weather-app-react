acc3433443cff9cb693b64945afc3941b48c284e
Bpmodele <Bpmodele@users.noreply.github.com>
2024-07-07 15:52:35 -0400

    Added rel and target

--- src/App.js
+++ src/App.js
@@ -9,9 +9,21 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project is coded by Madali Navas and is open-sourced on{" "}
-          <a href="https://github.com/Bpmodele/weather-app-react">GitHub</a> and
-          hosted on{" "}
-          <a href="https://madi-weather-react-app.netlify.app/">Netlify</a>
+          <a
+            href="https://github.com/Bpmodele/weather-app-react"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://madi-weather-react-app.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
